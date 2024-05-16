01862b82e21c1273322c2a650f47904bcfe5aa70
sharmakavita0262 <sharmakavita0262@gmail.com>
2024-05-16 18:47:08 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const mainRoute = require("./routes/main");
+
+const app = express();
+const PORT = process.env.PORT || 6000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to  backend server" });
+});
+
+app.use("/api", mainRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
